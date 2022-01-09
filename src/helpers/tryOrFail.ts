fe96e22fcb56a2c70989acc5a5627fa364a3d1d0
Bobby Johnson <bobby@madnesslabs.net>
2022-01-09 15:52:17 -0600

    fix(): Make events bubble and cancellable by default

--- src/helpers/tryOrFail.ts
+++ src/helpers/tryOrFail.ts
@@ -21,8 +21,8 @@ export default async function tryOrFail(
     event: options?.event?.detail?.event,
     name: options?.name,
     endpoint: options?.endpoint,
-    bubbles: !!options?.bubbles,
-    cancelable: !!options?.cancelable,
+    bubbles: !!options?.bubbles ?? true,
+    cancelable: !!options?.cancelable ?? true,
     composed: !!options?.composed,
   };
   try {
